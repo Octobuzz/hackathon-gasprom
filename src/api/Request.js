0733646de3d22bcc229bb220ba78a767380fbe6a
Никита Павленко <nick.pavlenko1998@gmail.com>
2020-06-18 22:42:08 +0300

    feat: initial commit

---
+++ src/api/Request.js
@@ -0,0 +1,10 @@
+import api from '../../config/api.json';
+import axios from 'axios';
+if (api.port.length === 0) {
+  axios.defaults.baseURL = `${api.host}/`;
+} else {
+  axios.defaults.baseURL = `${api.host}:${api.port}/`;
+}
+
+export default axios.create({
+});
