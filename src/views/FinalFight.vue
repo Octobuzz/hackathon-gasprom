740ac7fe1b80d29e1cf7546e5888e3baeba1371b
Оля <opastuk@yandex.ru>
2020-06-21 02:57:06 +0300

    feat: базовая верстка страницы битвы

--- src/views/FinalFight.vue
+++ src/views/FinalFight.vue
@@ -1,17 +1,42 @@
 <template>
-	<div>
-		Страница битвы
+	<div class="fight">
+		<page-header />
+		<h1 class="fight__headline">
+			Финальная битва!
+		</h1>
+		<div class="fight__wrapper">
+			<div class="fight__bankers" />
+			<div class="fight__ghosts" />
+		</div>
 	</div>
 </template>
 
 <script>
 import { Component, Vue } from 'vue-property-decorator';
+import Header from '../components/Header.vue';
 
-	@Component()
+	@Component({
+		components : {
+			'page-header': Header,
+		}
+	})
 export default class FinalFight extends Vue{
 	}
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+.fight {
+  padding-top: 60px;
+  background-color: $night;
+  background-image: url("../assets/fight.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  &__headline {
+    @include reset-text();
+    color: $white;
+    font-size: 50px;
+    margin: 35px 0;
+  }
+}
 </style>
